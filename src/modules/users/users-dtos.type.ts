07ceebbe4a40e2b622bc3a00fcbc052da4a294f2
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-11-27 17:57:12 +0200

    feat: define user dtos types

---
+++ src/modules/users/users-dtos.type.ts
@@ -0,0 +1,41 @@
+import {UserSortOptions} from "./user-sort-options.type";
+import {UserPermissions} from "./user-permissions.type";
+import {PaginationOptions} from "../../shared/pagination-options.type";
+import {UsersFilter} from "./users-filter.type";
+import {DateRangeFilter} from "../../shared/date-range-filter.type";
+
+export type GetUserDTO = { username: string };
+
+export type GetUsersDTO = {
+    filter?: UsersFilter,
+    dateRange?: DateRangeFilter,
+    sort?: UserSortOptions,
+    page?: PaginationOptions,
+};
+
+export type RegisterUserDTO = {
+    username: string,
+    password: string,
+};
+
+export type UpdateUserDTO = {
+    username: string,
+    newUsername?: string,
+    newPassword?: string,
+    newPermissions?: string[]
+};
+
+export type UpdateUsersDTO = {
+    filter: UsersFilter,
+    dateRange?: DateRangeFilter,
+    newPermissions: UserPermissions[],
+    sort?: UserSortOptions,
+};
+
+export type DeleteUserDTO = { username: string };
+
+export type DeleteUsersDTO = {
+    filter?: UsersFilter,
+    dateRange?: DateRangeFilter,
+    sort?: UserSortOptions,
+};

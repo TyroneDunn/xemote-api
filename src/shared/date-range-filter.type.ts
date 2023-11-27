7ac56441b903baa1f3cf590ac779907f48e7c91a
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-11-27 17:48:31 +0200

    feat: define date range filter type

---
+++ src/shared/date-range-filter.type.ts
@@ -0,0 +1,10 @@
+export type DateRangeFilter = {
+    dateCreated: {
+      startDate: string,
+      endDate: string,
+    },
+    lastUpdated: {
+        startDate: string,
+        endDate: string,
+    },
+};

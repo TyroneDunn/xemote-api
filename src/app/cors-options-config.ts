65a3c3a3e6fbe20fe884d4052c9c6d98d1fbc329
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-04 21:15:11 +0200

    feat: define cors options config

---
+++ src/app/cors-options-config.ts
@@ -0,0 +1,14 @@
+import {CorsOptions} from "cors";
+import {APP_URL} from "../environment/environment";
+import {OK} from "../shared/http-status-code.constant";
+
+export const corsOptions: CorsOptions = {
+    origin: [
+        '*',
+        APP_URL,
+    ],
+    allowedHeaders: 'Content-Type,credentials',
+    credentials: true,
+    optionsSuccessStatus: OK,
+    methods: ["GET", "POST", "DELETE", "PATCH"],
+};

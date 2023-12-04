846c63d412c3e04ab31fb103ae1bd406cef1190f
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-04 21:17:43 +0200

    feat: add app version environment variable

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -3,6 +3,7 @@ require('dotenv').config();
 export const NODE_ENV: string = process.env.NODE_ENV || 'development';
 export const API_TITLE: string = process.env.API_TITLE || 'xemote-api';
 export const API_PORT: number = parseInt(process.env.API_PORT || '2400');
+export const API_VERSION: string = process.env.API_VERSION || 'v1.0.0';
 export const APP_URL: string = process.env.APP_URL || '';
 export const MONGODB_URL: string = process.env.MONGO_DB_URL || '';
 export const SESSION_SECRET: string = process.env.SESSION_SECRET || '';

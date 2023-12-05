5b6c3797ac5ac8b82bb0e2aa8d596081081028eb
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-05 17:39:35 +0200

    refactor: replace request dependency with products dtos

--- src/modules/products/products-repository.type.ts
+++ src/modules/products/products-repository.type.ts
@@ -1,12 +1,21 @@
-// todo: remove hals dependency for pure logic
-import {Request} from "@hals/core"
 import {Product} from "./product.type";
+import {
+    GetProductDTO,
+    GetProductsDTO,
+    CreateProductDTO,
+    UpdateProductDTO,
+    UpdateProductsDTO,
+    DeleteProductDTO,
+    DeleteProductsDTO
+} from "./products-dtos.type";
 
 export type ProductsRepository = {
-    getProduct: (dto: Request, done: (product: Product) => void) => void,
-    getProducts: (dto: Request, done: (Products: Product[]) => void) => void,
-    createProduct: (dto: Request, done: (product: Product) => void) => void,
-    deleteProduct: (dto: Request, done: (product: Product) => void) => void,
-    updateProduct: (dto: Request, done: (product: Product) => void) => void,
-    exists: (dto: Request) => boolean,
+    getProduct: (dto: GetProductDTO, done: (product: Product) => void) => void,
+    getProducts: (dto: GetProductsDTO, done: (Products: Product[]) => void) => void,
+    createProduct: (dto: CreateProductDTO, done: (product: Product) => void) => void,
+    updateProduct: (dto: UpdateProductDTO, done: (product: Product) => void) => void,
+    updateProducts: (dto: UpdateProductsDTO, done: (products: Product[]) => void) => void,
+    deleteProduct: (dto: DeleteProductDTO, done: (product: Product) => void) => void,
+    deleteProducts: (dto: DeleteProductsDTO, done: (products: Product[]) => void) => void,
+    exists: (dto: GetProductDTO) => boolean,
 };

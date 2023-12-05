32f4021bca7665536598bb8989d7fc163d08a677
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-05 17:10:51 +0200

    refactor: update callback definition

--- src/modules/products/products-repository.type.ts
+++ src/modules/products/products-repository.type.ts
@@ -1,10 +1,12 @@
-import {Request, Response} from "@hals/core"
+// todo: remove hals dependency for pure logic
+import {Request} from "@hals/core"
+import {Product} from "./product.type";
 
 export type ProductsRepository = {
-    getProduct: (dto: Request) => Response,
-    getProducts: (dto: Request) => Response,
-    createProduct: (dto: Request) => Response,
-    deleteProduct: (dto: Request) => Response,
-    updateProduct: (dto: Request) => Response,
+    getProduct: (dto: Request, done: (product: Product) => void) => void,
+    getProducts: (dto: Request, done: (Products: Product[]) => void) => void,
+    createProduct: (dto: Request, done: (product: Product) => void) => void,
+    deleteProduct: (dto: Request, done: (product: Product) => void) => void,
+    updateProduct: (dto: Request, done: (product: Product) => void) => void,
     exists: (dto: Request) => boolean,
 };

616c27d70dd41cfd301d65f72e259a31ecc4f325
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-06 15:36:01 +0200

    refactor: add promise implementation

--- src/modules/products/products-dto-validator.utility.ts
+++ src/modules/products/products-dto-validator.utility.ts
@@ -8,31 +8,40 @@ import {
     UpdateProductDTO,
     UpdateProductsDTO,
 } from "./products-dtos.type";
+import {BadRequestError, NotFoundError} from "../../shared/errors.type";
+import {ProductsRepository} from "./products-repository.type";
+import {PRODUCTS_REPOSITORY} from "../../environment/repositories-config";
 
-export const validateGetProductDTO = (dto: GetProductDTO): ValidationOutcome => {
+const repository: ProductsRepository = PRODUCTS_REPOSITORY;
+
+export const validateGetProductDTO = async (dto: GetProductDTO): Promise<ValidationOutcome> => {
+    if (!dto._id)
+        return {error: new BadRequestError('Product ID  required.')};
+    if (!(await repository.exists(dto)))
+        return {error: new NotFoundError(`Product "${dto._id}" not found.`)};
+    return {};
+};
+
+export const validateGetProductsDTO = async (dto: GetProductsDTO): Promise<ValidationOutcome> => {
     return undefined;
 };
 
-export const validateGetProductsDTO = (dto: GetProductsDTO): ValidationOutcome => {
+export const validateCreateProductDTO = async (dto: CreateProductDTO): Promise<ValidationOutcome> => {
     return undefined;
 };
 
-export const validateCreateProductDTO = (dto: CreateProductDTO): ValidationOutcome => {
+export const validateUpdateProductDTO = async (dto: UpdateProductDTO): Promise<ValidationOutcome> => {
     return undefined;
 };
 
-export const validateUpdateProductDTO = (dto: UpdateProductDTO): ValidationOutcome => {
+export const validateUpdateProductsDTO = async (dto: UpdateProductsDTO): Promise<ValidationOutcome> => {
     return undefined;
 };
 
-export const validateUpdateProductsDTO = (dto: UpdateProductsDTO): ValidationOutcome => {
+export const validateDeleteProductDTO = async (dto: DeleteProductDTO): Promise<ValidationOutcome> => {
     return undefined;
 };
 
-export const validateDeleteProductDTO = (dto: DeleteProductDTO): ValidationOutcome => {
-    return undefined;
-};
-
-export const validateDeleteProductsDTO = (dto: DeleteProductsDTO): ValidationOutcome => {
+export const validateDeleteProductsDTO = async (dto: DeleteProductsDTO): Promise<ValidationOutcome> => {
     return undefined;
 };

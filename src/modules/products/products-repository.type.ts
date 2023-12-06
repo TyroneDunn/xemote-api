0ce6e5146ced59e4f265f08b787937b841e4e5b6
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-06 15:26:41 +0200

    refactor: replace callback with promise implementation

--- src/modules/products/products-repository.type.ts
+++ src/modules/products/products-repository.type.ts
@@ -10,12 +10,12 @@ import {
 } from "./products-dtos.type";
 
 export type ProductsRepository = {
-    getProduct: (dto: GetProductDTO, done: (product: Product) => void) => void,
-    getProducts: (dto: GetProductsDTO, done: (Products: Product[]) => void) => void,
-    createProduct: (dto: CreateProductDTO, done: (product: Product) => void) => void,
-    updateProduct: (dto: UpdateProductDTO, done: (product: Product) => void) => void,
-    updateProducts: (dto: UpdateProductsDTO, done: (products: Product[]) => void) => void,
-    deleteProduct: (dto: DeleteProductDTO, done: (product: Product) => void) => void,
-    deleteProducts: (dto: DeleteProductsDTO, done: (products: Product[]) => void) => void,
-    exists: (dto: GetProductDTO) => boolean,
+    getProduct: (dto: GetProductDTO) => Promise<Product>,
+    getProducts: (dto: GetProductsDTO) => Promise<Product[]>,
+    createProduct: (dto: CreateProductDTO) => Promise<Product>,
+    updateProduct: (dto: UpdateProductDTO) => Promise<Product>,
+    updateProducts: (dto: UpdateProductsDTO) => Promise<Product[]>,
+    deleteProduct: (dto: DeleteProductDTO) => Promise<Product>,
+    deleteProducts: (dto: DeleteProductsDTO) => Promise<Product[]>,
+    exists: (dto: GetProductDTO) => Promise<boolean>,
 };

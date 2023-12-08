5f2d7c96f80313ac7c39bec7970acab309c4ce0b
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-08 17:08:25 +0200

    fix: map request to get products dto

--- src/modules/products/products-dtos.utility.ts
+++ src/modules/products/products-dtos.utility.ts
@@ -19,19 +19,32 @@ export const mapRequestToGetProductDTO = (request: Request): GetProductDTO => ({
     _id: request.paramMap['id'],
 });
 
-export const mapRequestToGetProductsDTO = (request: Request): GetProductsDTO => ({
-    filter: {
-        name: request.queryParamMap['name'],
-        nameRegex: request.queryParamMap['nameRegex'],
-        type: request.queryParamMap['type'] as ProductType,
-        typeRegex: request.queryParamMap['typeRegex'],
-        costRange: JSON.parse(request.queryParamMap['costRange']) as NumberRange,
-        markupRange: JSON.parse(request.queryParamMap['markupRange']) as NumberRange,
-    },
-    dateRange: JSON.parse(request.queryParamMap['dateRange']) as DateRange,
-    sort: JSON.parse(request.queryParamMap['sort']) as ProductsSort[],
-    page: JSON.parse(request.queryParamMap['page']) as Pagination,
-});
+export const mapRequestToGetProductsDTO = (request: Request): GetProductsDTO =>
+    ({
+        filter: {
+            ...request.queryParamMap['name'] && {name: request.queryParamMap['name']},
+            ...request.queryParamMap['nameRegex'] && {nameRegex: request.queryParamMap['nameRegex']},
+            ...request.queryParamMap['type'] && {type: request.queryParamMap['type'] as ProductType},
+            ...request.queryParamMap['typeRegex'] && {typeRegex: request.queryParamMap['typeRegex']},
+            ...request.queryParamMap['costRange'] && {
+                costRange: {
+                    start: JSON.parse(request.queryParamMap['costRange']).start,
+                    end: JSON.parse(request.queryParamMap['costRange']).end,
+                }
+            },
+            ...request.queryParamMap['name'] && {markupRange: JSON.parse(request.queryParamMap['markupRange']) as NumberRange},
+        },
+        ...request.queryParamMap['dateRange'] && {
+            dateRange: {
+                ...(JSON.parse(request.queryParamMap['dateRange']) as DateRange).dateCreated &&
+                {dateCreated: (JSON.parse(request.queryParamMap['dateRange']) as DateRange).dateCreated},
+                ...(JSON.parse(request.queryParamMap['dateRange']) as DateRange).lastUpdated &&
+                {lastUpdated: (JSON.parse(request.queryParamMap['dateRange']) as DateRange).lastUpdated},
+            }
+        },
+        ...request.queryParamMap['sort'] && {sort: JSON.parse(request.queryParamMap['sort']) as ProductsSort[]},
+        ...request.queryParamMap['page'] && {page: JSON.parse(request.queryParamMap['page']) as Pagination},
+    });
 
 export const mapRequestToCreateProductDTO = (request: Request): CreateProductDTO => ({
     name: request.payload['name'],

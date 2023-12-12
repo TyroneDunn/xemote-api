0c94c952c6a28e6b74df0f3102d743f4901c73d8
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-12 23:23:41 +0200

    feat: declare inventory records dtos

---
+++ src/inventory/inventory-records-dtos.type.ts
@@ -0,0 +1,6 @@
+export type GetInventoryRecordDTO = {};
+export type InventoryRecordsDTO = {};
+export type CreateInventoryRecordDTO = {};
+export type UpdateInventoryRecordDTO = {};
+export type UpdateInventoryRecordsDTO = {};
+export type DeleteInventoryRecordDTO = {};

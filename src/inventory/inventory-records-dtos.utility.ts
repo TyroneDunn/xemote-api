a75348af13d2c8016b0cb28613ac9bd976eee767
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-13 23:45:28 +0200

    feat: implement map inventory record to success response

--- src/inventory/inventory-records-dtos.utility.ts
+++ src/inventory/inventory-records-dtos.utility.ts
@@ -1,6 +1,13 @@
-import {Request} from "@hals/core";
+import {HttpStatusCodes, Request, Response} from "@hals/core";
 import {GetInventoryRecordDTO,} from "./inventory-records-dtos.type";
+import {InventoryRecord} from "./inventory-record.type";
 
 export const mapRequestToGetInventoryRecordDTO = (request: Request): GetInventoryRecordDTO =>
     ({_id: request.paramMap['id']});
+
+export const mapInventoryRecordToSuccessResponse = (record: InventoryRecord): Response => ({
+    status: HttpStatusCodes.OK,
+    collection: [record],
+    count: 1
+});
 

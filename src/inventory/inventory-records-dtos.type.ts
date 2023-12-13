f1b826938139395272c288d9e33b2bfb8831b251
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-13 23:25:30 +0200

    feat: define inventory records dto type

--- src/inventory/inventory-records-dtos.type.ts
+++ src/inventory/inventory-records-dtos.type.ts
@@ -1,5 +1,28 @@
-export type GetInventoryRecordDTO = {};
-export type InventoryRecordsDTO = {};
+import {Timestamps} from "../shared/timestamps.type";
+import {Pagination} from "../shared/pagination.type";
+import {NumberRange} from "../shared/number-range.type";
+import {Order} from "../orders/order.type";
+
+
+export type InventoryRecordsDTO = {
+    filter?: InventoryRecordsFilter,
+    timestamps?: Timestamps,
+    sort?: InventoryRecordsSort,
+    page?: Pagination
+};
+
+export type InventoryRecordsFilter = {
+    countRange?: NumberRange,
+};
+
+export type InventoryRecordsSort = {
+    field: InventoryRecordsSortOption,
+    order: Order,
+};
+
+export type InventoryRecordsSortOption =
+    | "count";
+
 export type CreateInventoryRecordDTO = {};
 export type UpdateInventoryRecordDTO = {};
 export type UpdateInventoryRecordsDTO = {};

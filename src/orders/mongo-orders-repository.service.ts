fb1c64dd080fc6fb9efa1aed7f4a1bf0dec0a8ce
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-16 16:12:12 +0200

    feat: implement get orders

--- src/orders/mongo-orders-repository.service.ts
+++ src/orders/mongo-orders-repository.service.ts
@@ -15,7 +15,15 @@ export const MongoOrdersRepository: OrdersRepository = {
     getOrder: (dto: GetOrderDTO): Promise<Order> => OrdersModel.findById(dto._id),
 
     getOrders: (dto: OrdersDTO): Promise<Order[]> => {
-        return Promise.resolve([]);
+        const filter = mapOrdersDtoToFilter(dto);
+        const query = OrdersModel.find(filter);
+        if (dto.sort !== undefined)
+            query.sort({[dto.sort.field]: dto.sort.order === 'asc' ? 1 : -1});
+        if (dto.page !== undefined) {
+            query.skip(dto.page.index * dto.page.limit);
+            query.limit(dto.page.limit);
+        }
+        return query.exec();
     },
 
     createOrder: (dto: CreateOrderDTO): Promise<Order> =>
@@ -49,3 +57,43 @@ export const MongoOrdersRepository: OrdersRepository = {
         }
     }
 };
+
+const mapOrdersDtoToFilter = (dto: OrdersDTO) => ({
+    ...dto.filter.clientId && {clientId: dto.filter.clientId},
+    ...dto.filter.productId && {productId: dto.filter.productId},
+    ...dto.filter.status && {status: dto.filter.status},
+    ...dto.filter.countRange && {
+        ...(dto.filter.countRange.start && !dto.filter.countRange.end) && {count: {$gt: dto.filter.countRange.start}},
+        ...(!dto.filter.countRange.start && dto.filter.countRange.end) && {count: {$lt: dto.filter.countRange.end}},
+        ...(dto.filter.countRange.start && dto.filter.countRange.end) && {
+            count: {
+                $gt: dto.filter.countRange.start,
+                $lt: dto.filter.countRange.end
+            }
+        },
+    },
+    ...dto.timestamps && {
+        ...dto.timestamps.createdAt && {
+            ...(dto.timestamps.createdAt.start && !dto.timestamps.createdAt.end) && {
+                createdAt: {$gt: dto.timestamps.createdAt.start}
+            },
+            ...(!dto.timestamps.createdAt.start && dto.timestamps.createdAt.end) && {
+                createdAt: {$lt: dto.timestamps.createdAt.end}
+            },
+            ...(dto.timestamps.createdAt.start && dto.timestamps.createdAt.end) && {
+                createdAt: {$gt: dto.timestamps.createdAt.start, $lt: dto.timestamps.createdAt.end}
+            }
+        },
+        ...dto.timestamps.updatedAt && {
+            ...(dto.timestamps.updatedAt.start && !dto.timestamps.updatedAt.end) && {
+                updatedAt: {$gt: dto.timestamps.updatedAt.start}
+            },
+            ...(!dto.timestamps.updatedAt.start && dto.timestamps.updatedAt.end) && {
+                updatedAt: {$lt: dto.timestamps.updatedAt.end}
+            },
+            ...(dto.timestamps.updatedAt.start && dto.timestamps.updatedAt.end) && {
+                updatedAt: {$gt: dto.timestamps.updatedAt.start, $lt: dto.timestamps.updatedAt.end}
+            }
+        }
+    },
+});

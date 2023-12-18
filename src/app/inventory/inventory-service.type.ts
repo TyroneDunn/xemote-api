4eeff09460059dddf6c0ce1f29fe61d0faef7804
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-18 15:33:58 +0200

    refactor: simplify request handler definition

--- src/app/inventory/inventory-service.type.ts
+++ src/app/inventory/inventory-service.type.ts
@@ -1,11 +1,11 @@
-import {Request, Response} from "@hals/core";
+import {RequestHandler} from "@hals/core";
 
 export type InventoryService = {
-    getRecord: (request: Request) => Promise<Response>,
-    getRecords: (request: Request) => Promise<Response>,
-    createRecord: (request: Request) => Promise<Response>,
-    updateRecord: (request: Request) => Promise<Response>,
-    updateRecords: (request: Request) => Promise<Response>,
-    deleteRecord: (request: Request) => Promise<Response>,
-    deleteRecords: (request: Request) => Promise<Response>,
+    getRecord: RequestHandler,
+    getRecords: RequestHandler,
+    createRecord: RequestHandler,
+    updateRecord: RequestHandler,
+    updateRecords: RequestHandler,
+    deleteRecord: RequestHandler,
+    deleteRecords: RequestHandler,
 };

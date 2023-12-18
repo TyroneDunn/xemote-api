2338b527f4ae07291bf70ec5c5cb2343843ddca7
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-18 15:34:45 +0200

    refactor: simplify request handler definition

--- src/app/products/products-service.type.ts
+++ src/app/products/products-service.type.ts
@@ -1,11 +1,11 @@
-import {Request, Response} from "@hals/core";
+import {RequestHandler} from "@hals/core";
 
 export type ProductsService = {
-    getProduct: (request: Request) => Promise<Response>,
-    getProducts: (request: Request) => Promise<Response>,
-    createProduct: (request: Request) => Promise<Response>,
-    updateProduct: (request: Request) => Promise<Response>,
-    updateProducts: (request: Request) => Promise<Response>,
-    deleteProduct: (request: Request) => Promise<Response>,
-    deleteProducts: (request: Request) => Promise<Response>,
+    getProduct: RequestHandler,
+    getProducts: RequestHandler,
+    createProduct: RequestHandler,
+    updateProduct: RequestHandler,
+    updateProducts: RequestHandler,
+    deleteProduct: RequestHandler,
+    deleteProducts: RequestHandler,
 };

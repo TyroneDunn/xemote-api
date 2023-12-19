2dbc8ec467e3f00d24463c81dfd388dd155ca396
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-19 19:46:34 +0200

    chore: format

--- src/app/orders/orders-controller.utility.ts
+++ src/app/orders/orders-controller.utility.ts
@@ -1,86 +1,86 @@
-import {Controller, Method, RequestHandler} from "@hals/core";
-import {OrdersService} from "./orders-service.type";
+import { Controller, Method, RequestHandler } from "@hals/core";
+import { OrdersService } from "./orders-service.type";
 
 export const OrdersQueryParamsKeys: string[] = [
-    'clientId',
-    'status',
+   'clientId',
+   'status',
 ];
 
 const getOrdersMethod = (getOrders: RequestHandler): Method => ({
-    type: "GET",
-    paramKeys: [],
-    queryParamKeys: OrdersQueryParamsKeys,
-    sideEffects: [],
-    middleware: [],
-    requestHandler: getOrders
+   type: "GET",
+   paramKeys: [],
+   queryParamKeys: OrdersQueryParamsKeys,
+   sideEffects: [],
+   middleware: [],
+   requestHandler: getOrders,
 });
 
 const getOrderMethod = (getOrder: RequestHandler): Method => ({
-    type: "GET",
-    paramKeys: ['id'],
-    queryParamKeys: [],
-    sideEffects: [],
-    middleware: [],
-    requestHandler: getOrder
+   type: "GET",
+   paramKeys: [ 'id' ],
+   queryParamKeys: [],
+   sideEffects: [],
+   middleware: [],
+   requestHandler: getOrder,
 });
 
 const createOrderMethod = (createOrder: RequestHandler): Method => ({
-    type: "POST",
-    paramKeys: [],
-    queryParamKeys: [],
-    sideEffects: [],
-    middleware: [],
-    requestHandler: createOrder
+   type: "POST",
+   paramKeys: [],
+   queryParamKeys: [],
+   sideEffects: [],
+   middleware: [],
+   requestHandler: createOrder,
 });
 
 const updateOrdersMethod = (updateOrders: RequestHandler): Method => ({
-    type: "PATCH",
-    paramKeys: [],
-    queryParamKeys: OrdersQueryParamsKeys,
-    sideEffects: [],
-    middleware: [],
-    requestHandler: updateOrders
+   type: "PATCH",
+   paramKeys: [],
+   queryParamKeys: OrdersQueryParamsKeys,
+   sideEffects: [],
+   middleware: [],
+   requestHandler: updateOrders,
 });
 
 const updateOrderMethod = (updateOrder: RequestHandler): Method => ({
-    type: "PATCH",
-    paramKeys: ['id'],
-    queryParamKeys: [],
-    sideEffects: [],
-    middleware: [],
-    requestHandler: updateOrder
+   type: "PATCH",
+   paramKeys: [ 'id' ],
+   queryParamKeys: [],
+   sideEffects: [],
+   middleware: [],
+   requestHandler: updateOrder,
 });
 
 const deleteOrdersMethod = (deleteOrders: RequestHandler): Method => ({
-    type: "DELETE",
-    paramKeys: [],
-    queryParamKeys: OrdersQueryParamsKeys,
-    sideEffects: [],
-    middleware: [],
-    requestHandler: deleteOrders
+   type: "DELETE",
+   paramKeys: [],
+   queryParamKeys: OrdersQueryParamsKeys,
+   sideEffects: [],
+   middleware: [],
+   requestHandler: deleteOrders,
 });
 
 const deleteOrderMethod = (deleteOrder: RequestHandler): Method => ({
-    type: "DELETE",
-    paramKeys: ['id'],
-    queryParamKeys: [],
-    sideEffects: [],
-    middleware: [],
-    requestHandler: deleteOrder
-})
+   type: "DELETE",
+   paramKeys: [ 'id' ],
+   queryParamKeys: [],
+   sideEffects: [],
+   middleware: [],
+   requestHandler: deleteOrder,
+});
 
 export type OrdersController = Controller;
 
 export const configureOrdersController = (service: OrdersService): Controller => ({
-    path: 'orders/',
-    guard: true,
-    methods: [
-        getOrdersMethod(service.getOrders),
-        getOrderMethod(service.getOrder),
-        createOrderMethod(service.createOrder),
-        updateOrdersMethod(service.updateOrders),
-        updateOrderMethod(service.updateOrder),
-        deleteOrdersMethod(service.deleteOrders),
-        deleteOrderMethod(service.deleteOrder)
-    ]
+   path: 'orders/',
+   guard: true,
+   methods: [
+      getOrdersMethod(service.getOrders),
+      getOrderMethod(service.getOrder),
+      createOrderMethod(service.createOrder),
+      updateOrdersMethod(service.updateOrders),
+      updateOrderMethod(service.updateOrder),
+      deleteOrdersMethod(service.deleteOrders),
+      deleteOrderMethod(service.deleteOrder),
+   ],
 });

71a96ecf55e53a04d343a730ba907e9492a1d88b
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-19 22:44:25 +0200

    refactor: simplify dto dependency

--- src/app/users/users-repository.type.ts
+++ src/app/users/users-repository.type.ts
@@ -1,18 +1,17 @@
 import { User } from "./user.type";
 import {
    DeleteUserDTO,
-   DeleteUsersDTO,
    GetUserDTO,
-   GetUsersDTO,
    UpdateUserDTO,
    UpdateUsersDTO,
+   UsersDTO,
 } from "./users-dtos.type";
 
 export type UsersRepository = {
    getUser: (dto: GetUserDTO) => Promise<User>,
-   getUsers: (dto: GetUsersDTO) => Promise<User[]>,
+   getUsers: (dto: UsersDTO) => Promise<User[]>,
    deleteUser: (dto: DeleteUserDTO) => Promise<User>,
-   deleteUsers: (dto: DeleteUsersDTO) => Promise<string>,
+   deleteUsers: (dto: UsersDTO) => Promise<string>,
    updateUser: (dto: UpdateUserDTO) => Promise<User>,
    updateUsers: (dto: UpdateUsersDTO) => Promise<User[]>,
    exists: (username: string) => Promise<boolean>,

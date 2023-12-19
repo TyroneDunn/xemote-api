bafa2722882825826ffbc3a25c8656304e0c30d2
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-19 21:41:57 +0200

    refactor: simplify types

--- src/app/users/user.type.ts
+++ src/app/users/user.type.ts
@@ -1,12 +1,5 @@
-import { UserPermissions } from "./user-permissions.type";
-
 export type User = {
     _id: string,
     username: string,
     hash: string,
-    firstName: string,
-    lastName: string,
-    permissions: UserPermissions[],
-    dateCreated: Date,
-    lastUpdated: Date,
 };

22e9c1e499400adeceba3e3fa2b3c6debacada3a
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-19 23:31:47 +0200

    feat: implement map user to success response

--- src/app/users/users-dtos.utility.ts
+++ src/app/users/users-dtos.utility.ts
@@ -1,5 +1,13 @@
-import { Request } from "@hals/core";
+import { Request, Response } from "@hals/core";
 import { GetUserDTO } from "./users-dtos.type";
+import { User } from "./user.type";
+import { OK } from "@hals/common";
 
 export const mapRequestToGetUserDto = (request: Request): GetUserDTO =>
    ({ username: request.paramMap['username'] });
+
+export const mapUserToSuccessResponse = (user: User): Response => ({
+   status: OK,
+   collection: [ user ],
+   count: 1,
+});

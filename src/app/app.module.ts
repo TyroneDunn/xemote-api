0c467fe86d764afa4c56e0f836856c547968f14a
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-19 20:05:36 +0200

    chore: format

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,11 @@
-import {Application, buildSchema, newApplication, NodeEnvironmentOption, Schema} from "@hals/core";
-import {API_PORT, API_TITLE, API_VERSION, NODE_ENV} from "../environments";
+import {
+   Application,
+   buildSchema,
+   newApplication,
+   NodeEnvironmentOption,
+   Schema,
+} from "@hals/core";
+import { API_PORT, API_TITLE, API_VERSION, NODE_ENV } from "../environments";
 import corsOptions from "./cors-options-config";
 import authStrategy from "./auth-strategy-config";
 import productsController from "./products/products.module";
@@ -7,19 +13,19 @@ import inventoryController from "./inventory/inventory.module";
 import ordersController from "./orders/orders.module";
 
 export const schema: Schema = buildSchema(
-    `${NODE_ENV as NodeEnvironmentOption}`,
-    "Express",
-    `${API_TITLE}`,
-    `${API_VERSION}`,
-    API_PORT,
-    corsOptions,
-    authStrategy
+   `${NODE_ENV as NodeEnvironmentOption}`,
+   "Express",
+   `${API_TITLE}`,
+   `${API_VERSION}`,
+   API_PORT,
+   corsOptions,
+   authStrategy,
 );
 
 const app: Application = newApplication(schema, [
-    productsController,
-    inventoryController,
-    ordersController,
+   productsController,
+   inventoryController,
+   ordersController,
 ]);
 
 export default app;
--- src/app/cors-options-config.ts
+++ src/app/cors-options-config.ts
@@ -1,15 +1,15 @@
-import {APP_URL} from "../environments";
-import {CorsOptions, OK} from "@hals/common";
+import { APP_URL } from "../environments";
+import { CorsOptions, OK } from "@hals/common";
 
 const corsOptions: CorsOptions = {
-    origin: [
-        '*',
-        APP_URL,
-    ],
-    allowedHeaders: 'Content-Type,credentials',
-    credentials: true,
-    optionsSuccessStatus: OK,
-    methods: ["GET", "POST", "DELETE", "PATCH"],
+   origin: [
+      '*',
+      APP_URL,
+   ],
+   allowedHeaders: 'Content-Type,credentials',
+   credentials: true,
+   optionsSuccessStatus: OK,
+   methods: [ "GET", "POST", "DELETE", "PATCH" ],
 };
 
 export default corsOptions;

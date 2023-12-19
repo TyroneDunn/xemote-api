0afc6fb8b3c5e4af3c2083d778c7115a2d584b8c
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-19 21:57:08 +0200

    refactor: simplify

--- src/app/users/users-repository.type.ts
+++ src/app/users/users-repository.type.ts
@@ -1,26 +1,19 @@
 import { User } from "./user.type";
 import {
-    DeleteUserDTO,
-    DeleteUsersDTO,
-    GetUserDTO,
-    GetUsersDTO,
-    RegisterUserDTO,
-    UpdateUserDTO,
-    UpdateUsersDTO,
+   DeleteUserDTO,
+   DeleteUsersDTO,
+   GetUserDTO,
+   GetUsersDTO,
+   UpdateUserDTO,
+   UpdateUsersDTO,
 } from "./users-dtos.type";
-import { UserPermissions } from "./user-permissions.type";
 
 export type UsersRepository = {
-    getUser: (dto: GetUserDTO) => Promise<User>,
-    getUsers: (dto: GetUsersDTO) => Promise<User[]>,
-    registerUser: (dto: RegisterUserDTO) => Promise<User>,
-    deleteUser: (dto: DeleteUserDTO) => Promise<User>,
-    deleteUsers: (dto: DeleteUsersDTO) => Promise<string>,
-    updateUser: (dto: UpdateUserDTO) => Promise<User>,
-    updateUsers: (dto: UpdateUsersDTO) => Promise<User[]>,
-    exists: (username: string) => Promise<boolean>,
-    hasPermissions: (
-        username: string,
-        permissions: UserPermissions[]
-    ) => Promise<boolean>,
+   getUser: (dto: GetUserDTO) => Promise<User>,
+   getUsers: (dto: GetUsersDTO) => Promise<User[]>,
+   deleteUser: (dto: DeleteUserDTO) => Promise<User>,
+   deleteUsers: (dto: DeleteUsersDTO) => Promise<string>,
+   updateUser: (dto: UpdateUserDTO) => Promise<User>,
+   updateUsers: (dto: UpdateUsersDTO) => Promise<User[]>,
+   exists: (username: string) => Promise<boolean>,
 };

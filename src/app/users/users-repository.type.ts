b15397284ade2fc979a39c0bfd3d8227e55a5c02
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-19 23:23:48 +0200

    refactor: update command return type to command result

--- src/app/users/users-repository.type.ts
+++ src/app/users/users-repository.type.ts
@@ -6,13 +6,14 @@ import {
    UpdateUsersDTO,
    UsersDTO,
 } from "./users-dtos.type";
+import { CommandResult } from "@hals/common";
 
 export type UsersRepository = {
    getUser: (dto: GetUserDTO) => Promise<User>,
    getUsers: (dto: UsersDTO) => Promise<User[]>,
-   deleteUser: (dto: DeleteUserDTO) => Promise<User>,
-   deleteUsers: (dto: UsersDTO) => Promise<string>,
    updateUser: (dto: UpdateUserDTO) => Promise<User>,
-   updateUsers: (dto: UpdateUsersDTO) => Promise<User[]>,
+   updateUsers: (dto: UpdateUsersDTO) => Promise<CommandResult>,
+   deleteUser: (dto: DeleteUserDTO) => Promise<CommandResult>,
+   deleteUsers: (dto: UsersDTO) => Promise<CommandResult>,
    exists: (username: string) => Promise<boolean>,
 };

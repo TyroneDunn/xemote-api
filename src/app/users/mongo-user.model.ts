0abbd8f33d2d3e4b284284b593086efce4ff4e23
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-19 21:40:15 +0200

    refactor: use database name environment variable

--- src/app/users/mongo-user.model.ts
+++ src/app/users/mongo-user.model.ts
@@ -1,24 +1,25 @@
 import { Document, Schema } from 'mongoose';
 import { User } from "./user.type";
 import database from "../../database/mongodb.config";
+import { USERS_DB_NAME } from "../../environments";
 
 interface UserDocument extends Document, User {
-    _id: string,
-    username: string,
-    hash: string,
+   _id: string,
+   username: string,
+   hash: string,
 }
 
 const userSchema = new Schema<UserDocument>({
-    username: {
-        type: String,
-        unique: true,
-        required: true
-    },
-    hash: {
-        type: String,
-        required: true
-    },
+   username: {
+      type: String,
+      unique: true,
+      required: true,
+   },
+   hash: {
+      type: String,
+      required: true,
+   },
 });
 
-const UserModel = database.model<UserDocument>('User', userSchema);
+const UserModel = database.model<UserDocument>(USERS_DB_NAME, userSchema);
 export default UserModel;

25af02f2d337f2ac6dd1126ee2a31fab7a7b9a9e
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-19 19:47:03 +0200

    chore: format

--- src/app/orders/orders-dtos.utility.ts
+++ src/app/orders/orders-dtos.utility.ts
@@ -12,22 +12,22 @@ import { Order, OrderStatus, ProductCount } from "./order.type";
 import { mapRequestToPage, mapRequestToTimestamps, NumberRange, OrderOption } from "@hals/common";
 
 export const mapRequestToGetOrderDTO = (request: Request): GetOrderDTO =>
-   ( { _id: request.paramMap['id'] } );
+   ({ _id: request.paramMap['id'] });
 
-export const mapOrderToSuccessResponse = (order: Order): Response => ( {
+export const mapOrderToSuccessResponse = (order: Order): Response => ({
    status: HttpStatusCodes.OK,
    collection: [ order ],
    count: 1,
-} );
+});
 
-export const mapRequestToOrdersDTO = (request: Request): OrdersDTO => ( {
+export const mapRequestToOrdersDTO = (request: Request): OrdersDTO => ({
    ...mapRequestToOrdersFilter(request),
    ...mapRequestToTimestamps(request),
    ...mapRequestToOrdersSort(request),
    ...mapRequestToPage(request),
-} );
+});
 
-const mapRequestToOrdersFilter = (request: Request) => ( {
+const mapRequestToOrdersFilter = (request: Request) => ({
    filter: {
       ...request.queryParamMap['clientId'] && { clientId: request.queryParamMap['clientId'] },
       ...request.queryParamMap['productId'] && { productId: request.queryParamMap['productId'] },
@@ -36,48 +36,48 @@ const mapRequestToOrdersFilter = (request: Request) => ( {
       ...request.queryParamMap['countRange']
       && { countRange: JSON.parse(request.queryParamMap['countRange']) as NumberRange },
    },
-} );
+});
 
-const mapRequestToOrdersSort = (request: Request) => ( {
-   ...( request.queryParamMap['sort'] && request.queryParamMap['order'] ) && {
+const mapRequestToOrdersSort = (request: Request) => ({
+   ...(request.queryParamMap['sort'] && request.queryParamMap['order']) && {
       sort: {
          field: request.queryParamMap['sort'] as OrdersSortOptions,
          order: request.queryParamMap['order'] as OrderOption,
       },
    },
-} );
+});
 
-export const mapOrdersToSuccessResponse = (orders: Order[]): Response => ( {
+export const mapOrdersToSuccessResponse = (orders: Order[]): Response => ({
    status: HttpStatusCodes.OK,
    collection: [ orders ],
    count: orders.length,
-} );
+});
 
-export const mapRequestToCreateOrderDTO = (request: Request): CreateOrderDTO => ( {
+export const mapRequestToCreateOrderDTO = (request: Request): CreateOrderDTO => ({
    clientId: request.payload['clientId'],
    status: request.payload['status'] as OrderStatus,
    cart: request.payload['cart'] as ProductCount,
-} );
+});
 
-export const mapRequestToUpdateOrderDTO = (request: Request): UpdateOrderDTO => ( {
+export const mapRequestToUpdateOrderDTO = (request: Request): UpdateOrderDTO => ({
    _id: request.paramMap['id'],
    ...mapRequestToUpdateFields(request),
-} );
+});
 
-const mapRequestToUpdateFields = (request: Request) => ( {
+const mapRequestToUpdateFields = (request: Request) => ({
    updateFields: {
       ...request.payload['newStatus'] && { newStatus: request.payload['newStatus'] },
       ...request.payload['newCart'] && { newCart: request.payload['newCart'] as ProductCount },
    },
-} );
+});
 
-export const mapRequestToUpdateOrdersDTO = (request: Request): UpdateOrdersDTO => ( {
+export const mapRequestToUpdateOrdersDTO = (request: Request): UpdateOrdersDTO => ({
    ...mapRequestToOrdersFilter(request),
    ...mapRequestToTimestamps(request),
    ...mapRequestToOrdersSort(request),
    ...mapRequestToPage(request),
    ...mapRequestToUpdateFields(request),
-} );
+});
 
 export const mapRequestToDeleteOrderDTO = (request: Request): DeleteOrderDTO =>
-   ( { _id: request.paramMap['id'] } );
+   ({ _id: request.paramMap['id'] });

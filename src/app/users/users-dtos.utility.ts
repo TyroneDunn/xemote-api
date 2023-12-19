c3ec27b0ff05c75e3bd8e27d855c28500d747dd0
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-19 23:39:35 +0200

    feat: implement map request to users filter

--- src/app/users/users-dtos.utility.ts
+++ src/app/users/users-dtos.utility.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "@hals/core";
-import { GetUserDTO } from "./users-dtos.type";
+import { GetUserDTO, UsersDTO, UsersSortOption } from "./users-dtos.type";
 import { User } from "./user.type";
-import { OK } from "@hals/common";
+import { mapRequestToPage, mapRequestToTimestamps, OK, OrderOption } from "@hals/common";
 
 export const mapRequestToGetUserDto = (request: Request): GetUserDTO =>
    ({ username: request.paramMap['username'] });
@@ -11,3 +11,30 @@ export const mapUserToSuccessResponse = (user: User): Response => ({
    collection: [ user ],
    count: 1,
 });
+
+export const mapRequestToUsersDto = (request: Request): UsersDTO => ({
+   ...mapRequestToUsersFilter(request),
+   ...mapRequestToTimestamps(request),
+   ...mapRequestToUsersSort(request),
+   ...mapRequestToPage(request),
+});
+
+const mapRequestToUsersFilter = (request: Request) => ({
+   filter: {
+      ...request.queryParamMap['username']
+      && { username: request.queryParamMap['username'] },
+      ...request.queryParamMap['usernameRegex']
+      && { usernameRegex: request.queryParamMap['usernameRegex'] },
+   },
+});
+
+const mapRequestToUsersSort = (request: Request) => {
+   return {
+      ...(request.queryParamMap['sort'] && request.queryParamMap['order']) && {
+         sort: {
+            field: request.queryParamMap['sort'] as UsersSortOption,
+            order: request.queryParamMap['order'] as OrderOption,
+         },
+      },
+   };
+};

2924673bf9d6376d38d7bea33821d198b85fc78d
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-20 15:19:49 +0200

    chore: format

--- src/app/users/users-service.utility.ts
+++ src/app/users/users-service.utility.ts
@@ -26,51 +26,52 @@ import {
 export const configureUsersService = (
    repository: UsersRepository,
    validator: UsersDtosValidator,
-): UsersService => ({
-   getUser: async (request: Request): Promise<Response> => {
-      const dto: GetUserDTO = mapRequestToGetUserDto(request);
-      const validationOutcome: ValidationOutcome = await validator.validateGetUserDto(dto);
-      if (validationOutcome.error) return mapValidationOutcomeToErrorResponse(validationOutcome);
-      const result: User | Error = await repository.getUser(dto);
-      if (isError(result)) return mapErrorToInternalServerErrorResponse(result);
-      return mapUserToSuccessResponse(result);
-   },
+): UsersService =>
+   ({
+      getUser: async (request: Request): Promise<Response> => {
+         const dto: GetUserDTO = mapRequestToGetUserDto(request);
+         const validationOutcome: ValidationOutcome = await validator.validateGetUserDto(dto);
+         if (validationOutcome.error) return mapValidationOutcomeToErrorResponse(validationOutcome);
+         const result: User | Error = await repository.getUser(dto);
+         if (isError(result)) return mapErrorToInternalServerErrorResponse(result);
+         return mapUserToSuccessResponse(result);
+      },
 
-   getUsers: async (request: Request): Promise<Response> => {
-      const dto: UsersDTO = await mapRequestToUsersDto(request);
-      const validationOutcome: ValidationOutcome = await validator.validateUsersDto(dto);
-      if (validationOutcome.error) return mapValidationOutcomeToErrorResponse(validationOutcome);
-      const result: User[] | Error = await repository.getUsers(dto);
-      if (isError(result)) return mapErrorToInternalServerErrorResponse(result);
-      const addPageData = (response: Response): Response =>
-         addRequestPageDataToResponse(request, response);
-      return addPageData(mapUsersToSuccessResponse(result));
-   },
+      getUsers: async (request: Request): Promise<Response> => {
+         const dto: UsersDTO = await mapRequestToUsersDto(request);
+         const validationOutcome: ValidationOutcome = await validator.validateUsersDto(dto);
+         if (validationOutcome.error) return mapValidationOutcomeToErrorResponse(validationOutcome);
+         const result: User[] | Error = await repository.getUsers(dto);
+         if (isError(result)) return mapErrorToInternalServerErrorResponse(result);
+         const addPageData = (response: Response): Response =>
+            addRequestPageDataToResponse(request, response);
+         return addPageData(mapUsersToSuccessResponse(result));
+      },
 
-   updateUser: async (request: Request): Promise<Response> => {
-      const dto: UpdateUserDTO = mapRequestToUpdateUserDto(request);
-      const validationOutcome: ValidationOutcome = await validator.validateUpdateUserDto(dto);
-      if (validationOutcome.error) return mapValidationOutcomeToErrorResponse(validationOutcome);
-      const result: User | Error = await repository.updateUser(dto);
-      if (isError(result)) return mapErrorToInternalServerErrorResponse(result);
-      return mapUserToSuccessResponse(result);
-   },
+      updateUser: async (request: Request): Promise<Response> => {
+         const dto: UpdateUserDTO = mapRequestToUpdateUserDto(request);
+         const validationOutcome: ValidationOutcome = await validator.validateUpdateUserDto(dto);
+         if (validationOutcome.error) return mapValidationOutcomeToErrorResponse(validationOutcome);
+         const result: User | Error = await repository.updateUser(dto);
+         if (isError(result)) return mapErrorToInternalServerErrorResponse(result);
+         return mapUserToSuccessResponse(result);
+      },
 
-   deleteUser: async (request: Request): Promise<Response> => {
-      const dto: DeleteUserDTO = mapRequestToDeleteUserDto(request);
-      const validationOutcome: ValidationOutcome = await validator.validateDeleteUserDto(dto);
-      if (validationOutcome.error) return mapValidationOutcomeToErrorResponse(validationOutcome);
-      const result: CommandResult | Error = await repository.deleteUser(dto);
-      if (isError(result)) return mapErrorToInternalServerErrorResponse(result);
-      return mapDeleteResultToResponse(result);
-   },
+      deleteUser: async (request: Request): Promise<Response> => {
+         const dto: DeleteUserDTO = mapRequestToDeleteUserDto(request);
+         const validationOutcome: ValidationOutcome = await validator.validateDeleteUserDto(dto);
+         if (validationOutcome.error) return mapValidationOutcomeToErrorResponse(validationOutcome);
+         const result: CommandResult | Error = await repository.deleteUser(dto);
+         if (isError(result)) return mapErrorToInternalServerErrorResponse(result);
+         return mapDeleteResultToResponse(result);
+      },
 
-   deleteUsers: async (request: Request): Promise<Response> => {
-      const dto: UsersDTO = mapRequestToUsersDto(request);
-      const validationOutcome: ValidationOutcome = await validator.validateUsersDto(dto);
-      if (validationOutcome.error) return mapValidationOutcomeToErrorResponse(validationOutcome);
-      const result: CommandResult | Error = await repository.deleteUsers(dto);
-      if (isError(result)) return mapErrorToInternalServerErrorResponse(result);
-      return mapDeleteResultToResponse(result);
-   },
-});
+      deleteUsers: async (request: Request): Promise<Response> => {
+         const dto: UsersDTO = mapRequestToUsersDto(request);
+         const validationOutcome: ValidationOutcome = await validator.validateUsersDto(dto);
+         if (validationOutcome.error) return mapValidationOutcomeToErrorResponse(validationOutcome);
+         const result: CommandResult | Error = await repository.deleteUsers(dto);
+         if (isError(result)) return mapErrorToInternalServerErrorResponse(result);
+         return mapDeleteResultToResponse(result);
+      },
+   });

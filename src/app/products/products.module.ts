a62d7d10baf959fd8a4c65d42ba1e17db7d2f5ec
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-01-05 18:25:44 +0200

    chore: format

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,20 +1,14 @@
 import { ProductsDtosValidator } from "./products-dtos.validator";
 import { ProductsController } from "./products.controller";
 import { ProductsService } from "./products.service";
-import { ProductsRepository } from "./products-repository.type";
 import { MongoProductsRepository } from "./mongo-products-repository.service";
-import { InventoryRepository } from "../inventory/inventory-repository.type";
 import { MongoInventoryRepository } from "../inventory/mongo-inventory-repository.service";
 
-const productsRepository : ProductsRepository = MongoProductsRepository;
-const inventoryRepository : InventoryRepository = MongoInventoryRepository;
-const productsDtoValidator : ProductsDtosValidator = ProductsDtosValidator(productsRepository);
-const productsService : ProductsService =
-   ProductsService(
-      productsRepository,
-      productsDtoValidator,
-      inventoryRepository,
-   );
+const productsService : ProductsService = ProductsService(
+   MongoProductsRepository,
+   ProductsDtosValidator(MongoProductsRepository),
+   MongoInventoryRepository,
+);
 const productsController : ProductsController = ProductsController(productsService);
 
 export default productsController;

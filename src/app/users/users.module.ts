43970c3db651f65129f6b0a751967cd336e5ea91
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-01-06 10:31:34 +0200

    refactor: simplify definition

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,13 +1,11 @@
 import { configureUsersController } from "./users.controller";
 import { MongoUsersRepository } from "./mongo-users-repository.service";
-import { UsersRepository } from "./users-repository.type";
 import { UsersValidator } from "./users.validator";
 import { configureUsersService, UsersService } from "./users.service";
 import { Controller } from '@hals/common';
 
-const usersRepository: UsersRepository = MongoUsersRepository;
-const usersDtosValidator: UsersValidator = UsersValidator(usersRepository);
-const usersService: UsersService = configureUsersService(usersRepository, usersDtosValidator);
+const usersValidator: UsersValidator = UsersValidator(MongoUsersRepository);
+const usersService: UsersService = configureUsersService(MongoUsersRepository, usersValidator);
 const usersController: Controller = configureUsersController(usersService);
 
 export default usersController;

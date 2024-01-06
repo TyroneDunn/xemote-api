b0eb71e85e97df1d7d7758bd9749b9c5439cc31c
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-01-06 17:10:08 +0200

    refactor: use validation error value constructor

--- src/app/inventory/inventory-records.validator.ts
+++ src/app/inventory/inventory-records.validator.ts
@@ -19,14 +19,12 @@ export const InventoryRecordsValidator =
    (inventoryRepository: InventoryRepository): InventoryRecordsValidator => ({
       validateGetInventoryRecordRequest: async (dto: GetInventoryRecordRequest): Promise<ValidationError | null> => {
          if (!dto.productId)
-            return { error: { type: "BadRequest", message: 'Product ID required.' } };
+            return ValidationError("BadRequest", 'Product ID required.');
+
          if (!(await inventoryRepository.exists(dto)))
-            return {
-               error: {
-                  type: "NotFound",
-                  message: `Product inventory record "${dto.productId}" not found.`,
-               },
-            };
+            return ValidationError("NotFound",
+               `Product inventory record "${dto.productId}" not found.`);
+
          return null;
       },
 
@@ -34,125 +32,73 @@ export const InventoryRecordsValidator =
          if (dto.filter) {
             if (dto.filter.countRange) {
                if (dto.filter.countRange.start && (dto.filter.countRange.start < 0))
-                  return {
-                     error: {
-                        type: "BadRequest", message: 'Invalid count range. Count range' +
-                           ' start value must be greater than 0.',
-                     },
-                  };
+                  return ValidationError("BadRequest", 'Invalid count range. Count range' +
+                           ' start value must be greater than 0.');
+
                if (dto.filter.countRange.end && (dto.filter.countRange.end < 0))
-                  return {
-                     error: {
-                        type: "BadRequest", message: 'Invalid count range. Count range' +
-                           ' end value must be greater than 0.',
-                     },
-                  };
+                  return ValidationError("BadRequest", 'Invalid count range. Count range' +
+                           ' end value must be greater than 0.');
+
                if ((dto.filter.countRange.start && dto.filter.countRange.end)
                   && (dto.filter.countRange.end < dto.filter.countRange.start))
-                  return {
-                     error: {
-                        type: "BadRequest", message: 'Invalid count range. Count range' +
-                           ' end value must be greater than start value.',
-                     },
-                  };
+                  return ValidationError("BadRequest", 'Invalid count range. Count range' +
+                           ' end value must be greater than start value.');
             }
          }
 
          if (dto.timestamps) {
             if (dto.timestamps.createdAt) {
                if (dto.timestamps.createdAt.start && isNaN(Date.parse(dto.timestamps.createdAt.start)))
-                  return {
-                     error: {
-                        type: "BadRequest", message: 'Invalid createdAt start' +
-                           ' date. Provide a valid ISO date string.',
-                     },
-                  };
+                  return ValidationError("BadRequest", 'Invalid createdAt start' +
+                           ' date. Provide a valid ISO date string.');
+
                if (dto.timestamps.createdAt.end && isNaN(Date.parse(dto.timestamps.createdAt.end)))
-                  return {
-                     error: {
-                        type: "BadRequest", message: 'Invalid createdAt end' +
-                           ' date. Provide a valid ISO date string.',
-                     },
-                  };
+                  return ValidationError("BadRequest", 'Invalid createdAt end' +
+                           ' date. Provide a valid ISO date string.');
             }
+
             if (dto.timestamps.updatedAt) {
                if (dto.timestamps.updatedAt.start && isNaN(Date.parse(dto.timestamps.updatedAt.start)))
-                  return {
-                     error: {
-                        type: "BadRequest", message: 'Invalid updatedAt start' +
-                           ' date. Provide a valid ISO date string.',
-                     },
-                  };
+                  return ValidationError("BadRequest", 'Invalid updatedAt start' +
+                           ' date. Provide a valid ISO date string.');
+
                if (dto.timestamps.updatedAt.end && isNaN(Date.parse(dto.timestamps.updatedAt.end)))
-                  return {
-                     error: {
-                        type: "BadRequest", message: 'Invalid updatedAt end' +
-                           ' date. Provide a valid ISO date string.',
-                     },
-                  };
+                  return ValidationError("BadRequest", 'Invalid updatedAt end' +
+                           ' date. Provide a valid ISO date string.');
             }
          }
 
          if (dto.sort) {
             if (dto.sort.field && !dto.sort.order)
-               return {
-                  error: {
-                     type: "BadRequest",
-                     message: 'Invalid sort. Provide sort order.',
-                  },
-               };
+               return ValidationError("BadRequest", 'Invalid sort. Provide sort order.');
+
             if (!dto.sort.field && dto.sort.order)
-               return {
-                  error: {
-                     type: "BadRequest",
-                     message: 'Invalid sort. Provide sort field.',
-                  },
-               };
+               return ValidationError("BadRequest", 'Invalid sort. Provide sort field.');
+
             if (dto.sort.field !== "count")
-               return {
-                  error: {
-                     type: "BadRequest", message: 'Invalid sort. Sort field must be' +
-                        ' "count".',
-                  },
-               };
+               return ValidationError("BadRequest", 'Invalid sort. Sort field must be' +
+                        ' "count".');
+
             if (dto.sort.order !== "asc" && dto.sort.order !== "desc")
-               return {
-                  error: {
-                     type: "BadRequest", message: 'Invalid sort. Sort order must be' +
-                        ' "asc" or "desc".',
-                  },
-               };
+               return ValidationError("BadRequest", 'Invalid sort. Sort order must be' +
+                        ' "asc" or "desc".');
          }
 
          if (dto.page) {
             if (dto.page.index && !dto.page.limit)
-               return {
-                  error: {
-                     type: "BadRequest",
-                     message: 'Invalid page. Provide page limit.',
-                  },
-               };
+               return ValidationError("BadRequest", 'Invalid page. Provide page limit.');
+
             if (!dto.page.index && dto.page.limit)
-               return {
-                  error: {
-                     type: "BadRequest",
-                     message: 'Invalid page. Provide page index.',
-                  },
-               };
+               return ValidationError("BadRequest",
+                     'Invalid page. Provide page index.');
+
             if (dto.page.index < 0)
-               return {
-                  error: {
-                     type: "BadRequest", message: 'Invalid page. Page index must be' +
-                        ' 0 or greater.',
-                  },
-               };
+               return ValidationError("BadRequest", 'Invalid page. Page index must be' +
+                        ' 0 or greater.');
+
             if (dto.page.limit < 1)
-               return {
-                  error: {
-                     type: "BadRequest", message: 'Invalid page. Page limit must be' +
-                        ' 1 or greater.',
-                  },
-               };
+               return ValidationError("BadRequest", 'Invalid page. Page limit must be' +
+                        ' 1 or greater.');
          }
 
          return null;
@@ -160,41 +106,29 @@ export const InventoryRecordsValidator =
 
       validateUpdateInventoryRecordRequest: async (dto: UpdateInventoryRecordRequest): Promise<ValidationError | null> => {
          if (!dto.productId)
-            return { error: { type: "BadRequest", message: 'ID required.' } };
+            return ValidationError("BadRequest", 'ID required.');
+
          if (!(await inventoryRepository.exists(dto)))
-            return {
-               error: {
-                  type: "NotFound",
-                  message: `Inventory record "${dto.productId}" not found.`,
-               },
-            };
+            return ValidationError("NotFound",
+                  `Inventory record "${dto.productId}" not found.`);
+
          if (!dto.updateFields)
-            return {
-               error: {
-                  type: "BadRequest", message: 'Invalid request. Update field(s)' +
-                     ' required.',
-               },
-            };
+            return ValidationError("BadRequest", 'Invalid request. Update field(s)' +
+                     ' required.');
+
          if (dto.updateFields.newCount)
             if (dto.updateFields.newCount < 0)
-               return {
-                  error: {
-                     type: "BadRequest", message: 'Invalid count. Count must be' +
-                        ' greater than 0.',
-                  },
-               };
+               return ValidationError("BadRequest", 'Invalid count. Count must be' +
+                        ' greater than 0.');
+
          if (dto.updateFields.countDelta) {
             const result: InventoryRecord | Error = await inventoryRepository.getRecord({ productId: dto.productId });
-            if (isError(result)) return {
-               error: { type: "Internal", message: 'Repository read error.' },
-            };
+            if (isError(result)) return ValidationError("Internal", 'Repository read' +
+            ' error.');
+
             if ((result.count + dto.updateFields.countDelta) < 0)
-               return {
-                  error: {
-                     type: "BadRequest", message: 'Invalid count delta. Not' +
-                        ' enough products.',
-                  },
-               };
+               return ValidationError("BadRequest", 'Invalid count delta. Not' +
+                        ' enough products.');
          }
          return null;
       },
@@ -202,86 +136,53 @@ export const InventoryRecordsValidator =
       validateUpdateInventoryRecordsRequest: async (dto: UpdateInventoryRecordsRequest): Promise<ValidationError | null> => {
          if (dto.filter.countRange) {
             if (dto.filter.countRange.start && (dto.filter.countRange.start < 0))
-               return {
-                  error: {
-                     type: "BadRequest", message: 'Invalid count range. Count range' +
-                        ' start value must be greater than 0.',
-                  },
-               };
+               return ValidationError("BadRequest", 'Invalid count range. Count range' +
+                        ' start value must be greater than 0.');
+
             if (dto.filter.countRange.end && (dto.filter.countRange.end < 0))
-               return {
-                  error: {
-                     type: "BadRequest", message: 'Invalid count range. Count range' +
-                        ' end value must be greater than 0.',
-                  },
-               };
+               return ValidationError("BadRequest", 'Invalid count range. Count range' +
+                        ' end value must be greater than 0.');
+
             if ((dto.filter.countRange.start && dto.filter.countRange.end)
                && (dto.filter.countRange.end < dto.filter.countRange.start))
-               return {
-                  error: {
-                     type: "BadRequest", message: 'Invalid count range. Count range' +
-                        ' end value must be greater than start value.',
-                  },
-               };
+               return ValidationError("BadRequest", 'Invalid count range. Count range' +
+                        ' end value must be greater than start value.');
          }
 
          if (dto.timestamps) {
             if (dto.timestamps.createdAt) {
                if (dto.timestamps.createdAt.start && isNaN(Date.parse(dto.timestamps.createdAt.start)))
-                  return {
-                     error: {
-                        type: "BadRequest", message: 'Invalid createdAt start' +
-                           ' date. Provide a valid ISO date string.',
-                     },
-                  };
+                  return ValidationError("BadRequest", 'Invalid createdAt start' +
+                           ' date. Provide a valid ISO date string.');
+
                if (dto.timestamps.createdAt.end && isNaN(Date.parse(dto.timestamps.createdAt.end)))
-                  return {
-                     error: {
-                        type: "BadRequest", message: 'Invalid createdAt end' +
-                           ' date. Provide a valid ISO date string.',
-                     },
-                  };
+                  return ValidationError("BadRequest", 'Invalid createdAt end' +
+                           ' date. Provide a valid ISO date string.');
             }
+
             if (dto.timestamps.updatedAt) {
                if (dto.timestamps.updatedAt.start && isNaN(Date.parse(dto.timestamps.updatedAt.start)))
-                  return {
-                     error: {
-                        type: "BadRequest", message: 'Invalid updatedAt start' +
-                           ' date. Provide a valid ISO date string.',
-                     },
-                  };
+                  return ValidationError("BadRequest", 'Invalid updatedAt start' +
+                           ' date. Provide a valid ISO date string.');
+
                if (dto.timestamps.updatedAt.end && isNaN(Date.parse(dto.timestamps.updatedAt.end)))
-                  return {
-                     error: {
-                        type: "BadRequest", message: 'Invalid updatedAt end' +
-                           ' date. Provide a valid ISO date string.',
-                     },
-                  };
+                  return ValidationError("BadRequest", 'Invalid updatedAt end' +
+                           ' date. Provide a valid ISO date string.');
             }
          }
 
          if (!dto.updateFields)
-            return {
-               error: {
-                  type: "BadRequest", message: 'Invalid request. Update field(s)' +
-                     ' required.',
-               },
-            };
+            return ValidationError("BadRequest", 'Invalid request. Update field(s)' +
+                     ' required.');
+
          if (dto.updateFields.newCount)
             if (dto.updateFields.newCount < 0)
-               return {
-                  error: {
-                     type: "BadRequest", message: 'Invalid count. Count must be' +
-                        ' greater than 0.',
-                  },
-               };
+               return ValidationError("BadRequest", 'Invalid count. Count must be' +
+                        ' greater than 0.');
+
          if (dto.updateFields.countDelta)
-            return {
-               error: {
-                  type: "BadRequest", message: 'Invalid update. Cannot update' +
-                     ' multiple records with count delta.',
-               },
-            };
+            return ValidationError("BadRequest", 'Invalid update. Cannot update' +
+                     ' multiple records with count delta.');
 
          return null;
       },

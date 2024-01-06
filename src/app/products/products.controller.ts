c1fa721c1494be642a162db44f6cf0e628377b90
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-01-06 10:05:33 +0200

    chore: format

--- src/app/products/products.controller.ts
+++ src/app/products/products.controller.ts
@@ -1,7 +1,48 @@
 import { Controller, Method, RequestHandler } from "@hals/common";
 import { ProductsService } from "./products.service";
 
-export const ProductsQueryParamKeys: string[] = [
+const getProductsMethod = (getProducts : RequestHandler) : Method => ({
+   type           : "GET",
+   queryParamKeys : ProductsQueryParamKeys,
+   requestHandler : getProducts,
+});
+
+const getProductMethod = (getProduct : RequestHandler) : Method => ({
+   type           : "GET",
+   paramKeys      : [ 'id' ],
+   requestHandler : getProduct,
+});
+
+const createProductMethod = (createProduct : RequestHandler) : Method => ({
+   type           : "POST",
+   requestHandler : createProduct,
+});
+
+const updateProductsMethod = (updateProducts : RequestHandler) : Method => ({
+   type           : "PATCH",
+   queryParamKeys : ProductsQueryParamKeys,
+   requestHandler : updateProducts,
+});
+
+const updateProductMethod = (updateProduct : RequestHandler) : Method => ({
+   type           : "PATCH",
+   paramKeys      : [ 'id' ],
+   requestHandler : updateProduct,
+});
+
+const deleteProductsMethod = (deleteProducts : RequestHandler) : Method => ({
+   type           : "DELETE",
+   queryParamKeys : ProductsQueryParamKeys,
+   requestHandler : deleteProducts,
+});
+
+const deleteProductMethod = (deleteProduct : RequestHandler) : Method => ({
+   type           : "DELETE",
+   paramKeys      : [ 'id' ],
+   requestHandler : deleteProduct,
+});
+
+const ProductsQueryParamKeys : string[] = [
    'name',
    'nameRegex',
    'category',
@@ -16,51 +57,10 @@ export const ProductsQueryParamKeys: string[] = [
    'limit',
 ];
 
-const getProductsMethod = (getProducts: RequestHandler): Method => ({
-   type: "GET",
-   queryParamKeys: ProductsQueryParamKeys,
-   requestHandler: getProducts,
-});
-
-const getProductMethod = (getProduct: RequestHandler): Method => ({
-   type: "GET",
-   paramKeys: [ 'id' ],
-   requestHandler: getProduct,
-});
-
-const createProductMethod = (createProduct: RequestHandler): Method => ({
-   type: "POST",
-   requestHandler: createProduct,
-});
-
-const updateProductsMethod = (updateProducts: RequestHandler): Method => ({
-   type: "PATCH",
-   queryParamKeys: ProductsQueryParamKeys,
-   requestHandler: updateProducts,
-});
-
-const updateProductMethod = (updateProduct: RequestHandler): Method => ({
-   type: "PATCH",
-   paramKeys: [ 'id' ],
-   requestHandler: updateProduct,
-});
-
-const deleteProductsMethod = (deleteProducts: RequestHandler): Method => ({
-   type: "DELETE",
-   queryParamKeys: ProductsQueryParamKeys,
-   requestHandler: deleteProducts,
-});
-
-const deleteProductMethod = (deleteProduct: RequestHandler): Method => ({
-   type: "DELETE",
-   paramKeys: [ 'id' ],
-   requestHandler: deleteProduct,
-});
-
 export const ProductsController = (service : ProductsService) : Controller => ({
-   path: 'products/',
-   guard: true,
-   methods: [
+   path    : 'products/',
+   guard   : true,
+   methods : [
       getProductsMethod(service.getProducts),
       getProductMethod(service.getProduct),
       createProductMethod(service.createProduct),

f32a79dd72695b7f4fa92b4b504bb58ba1b7b7e7
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-01-06 11:41:17 +0200

    refactor: simplify orders module definition

--- src/app/orders/orders.module.ts
+++ src/app/orders/orders.module.ts
@@ -1,16 +1,12 @@
 import { configureOrdersController, OrdersController } from "./orders-controller.utility";
 import { OrdersService } from "./orders-service.type";
 import { configureOrdersService } from "./orders-service.utility";
-import { OrdersRepository } from "./orders-repository.type";
 import { MongoOrdersRepository } from "./mongo-orders-repository.service";
 import { configureOrdersDtosValidator, OrdersDtosValidator } from "./orders-dtos-validator.utility";
-import { ProductsRepository } from "../products/products-repository.type";
 import { MongoProductsRepository } from "../products/mongo-products-repository.service";
 
-const ordersRepository: OrdersRepository = MongoOrdersRepository;
-const productsRepository: ProductsRepository = MongoProductsRepository;
-const ordersDtosValidator: OrdersDtosValidator = configureOrdersDtosValidator(ordersRepository, productsRepository);
-const ordersService: OrdersService = configureOrdersService(ordersRepository, ordersDtosValidator);
+const ordersDtosValidator: OrdersDtosValidator = configureOrdersDtosValidator(MongoOrdersRepository, MongoProductsRepository);
+const ordersService: OrdersService = configureOrdersService(MongoOrdersRepository, ordersDtosValidator);
 const ordersController: OrdersController = configureOrdersController(ordersService);
 
 export default ordersController;

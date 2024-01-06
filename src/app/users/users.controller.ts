4e8fbb1bf5e89d006a3669571b247340d082a129
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-01-06 10:41:14 +0200

    refactor: simplify methods definitions

--- src/app/users/users.controller.ts
+++ src/app/users/users.controller.ts
@@ -1,56 +1,40 @@
 import { Controller, Method, RequestHandler } from "@hals/common";
-
 import { UsersService } from './users.service';
-
-const UserQueryParamKeys: string[] = [
-   'username',
-   'usernameRegex',
-];
 
 const getUsersMethod = (getUsers: RequestHandler): Method => ({
    type: "GET",
-   paramKeys: [],
    queryParamKeys: UserQueryParamKeys,
-   sideEffects: [],
-   middleware: [],
    requestHandler: getUsers,
 });
 
 const getUserMethod = (getUser: RequestHandler): Method => ({
    type: "GET",
    paramKeys: [ 'username' ],
-   queryParamKeys: [],
-   sideEffects: [],
-   middleware: [],
    requestHandler: getUser,
 });
 
 const updateUserMethod = (updateUser: RequestHandler): Method => ({
    type: "PATCH",
    paramKeys: [ 'username' ],
-   queryParamKeys: [],
-   sideEffects: [],
-   middleware: [],
    requestHandler: updateUser,
 });
 
 const deleteUsersMethod = (deleteUsers: RequestHandler): Method => ({
    type: "DELETE",
-   paramKeys: [],
    queryParamKeys: UserQueryParamKeys,
-   sideEffects: [],
-   middleware: [],
    requestHandler: deleteUsers,
 });
 
 const deleteUserMethod = (deleteUser: RequestHandler): Method => ({
    type: "DELETE",
    paramKeys: [ 'username' ],
-   queryParamKeys: [],
-   sideEffects: [],
-   middleware: [],
    requestHandler: deleteUser,
 });
+
+const UserQueryParamKeys: string[] = [
+   'username',
+   'usernameRegex',
+];
 
 export const configureUsersController = (service: UsersService): Controller => ({
    path: 'users/',

ea503998a2765e7c8ab84f8a6b5de6c79635da68
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-01-26 19:21:28 +0200

    refactor: hide all but products controller

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,7 @@ import { hals } from "@hals/core";
 import { API_PORT, API_TITLE, API_VERSION, NODE_ENV } from "../environments";
 import corsOptions from "./cors-options-config";
 import authStrategy from "./auth-strategy-config";
-import usersController from "./users/users.module";
 import productsController from "./products/products.module";
-import inventoryController from "./inventory/inventory.module";
-import ordersController from "./orders/orders.module";
 import { Application, ApplicationSchema } from "@hals/common";
 import { NodeEnvironmentOption } from "@hals/common/lib/app/application-schema.type";
 
@@ -20,10 +17,10 @@ export const appSchema: ApplicationSchema = {
 };
 
 const app: Application = hals(appSchema, [
-   usersController,
+   // usersController,
    productsController,
-   inventoryController,
-   ordersController,
+   // inventoryController,
+   // ordersController,
 ]);
 
 export default app;

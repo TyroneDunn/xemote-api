77720fd0ed6555e5b64fd48060462baa7e5c493a
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-02-01 13:05:33 +0200

    refactor: add log request side effects

--- src/app/products/products.controller.ts
+++ src/app/products/products.controller.ts
@@ -1,44 +1,57 @@
 import { Controller, Method, RequestHandler } from "@hals/common";
 import { ProductsService } from "./products.service";
+import {
+   logCreateRequest,
+   logDeleteRequest,
+   logGetRequest,
+   logPatchRequest,
+} from '../log/log.utility';
 
 const getProductsMethod = (getProducts : RequestHandler) : Method => ({
    type           : "GET",
    queryParamKeys : ProductsQueryParamKeys,
+   sideEffects    : [ logGetRequest("Get Products") ],
    requestHandler : getProducts,
 });
 
 const getProductMethod = (getProduct : RequestHandler) : Method => ({
    type           : "GET",
    paramKeys      : [ ID ],
+   sideEffects    : [ logGetRequest("Get Product") ],
    requestHandler : getProduct,
 });
 
 const createProductMethod = (createProduct : RequestHandler) : Method => ({
    type           : "POST",
+   sideEffects    : [ logCreateRequest("Create Product") ],
    requestHandler : createProduct,
 });
 
 const updateProductsMethod = (updateProducts : RequestHandler) : Method => ({
    type           : "PATCH",
    queryParamKeys : ProductsQueryParamKeys,
+   sideEffects    : [ logPatchRequest("Patch Products") ],
    requestHandler : updateProducts,
 });
 
 const updateProductMethod = (updateProduct : RequestHandler) : Method => ({
    type           : "PATCH",
    paramKeys      : [ ID ],
+   sideEffects    : [ logPatchRequest("Patch Product") ],
    requestHandler : updateProduct,
 });
 
 const deleteProductsMethod = (deleteProducts : RequestHandler) : Method => ({
    type           : "DELETE",
    queryParamKeys : ProductsQueryParamKeys,
+   sideEffects    : [ logDeleteRequest("Delete Products") ],
    requestHandler : deleteProducts,
 });
 
 const deleteProductMethod = (deleteProduct : RequestHandler) : Method => ({
    type           : "DELETE",
    paramKeys      : [ ID ],
+   sideEffects    : [ logDeleteRequest("Delete Product") ],
    requestHandler : deleteProduct,
 });
 
